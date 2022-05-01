162a4cc6784b2365efc9526806c9d5f791994b74
Anna Shulyak <ourmac@Ours-iMac.fios-router.home>
2022-05-01 13:29:06 -0400

    adding form handling and POST

--- js/index.js
+++ js/index.js
@@ -7,11 +7,13 @@ let pageNum = '1'
 
 
 function getData() {
+    console.log(document.querySelector('#monster-container'.innerHTML))
     fetch(`${dataURL}${pageLimit}${pageParam}${pageNum}`).then(res => res.json()).then(data => {
         console.log(data)
         const monsterList = document.querySelector('#monster-container')
         console.log(monsterList)
         data.forEach((monster) => {
+            console.log(document.querySelector('#monster-container'.childNodes))
             monsterList.append(createMonster(monster))
         })
     })
@@ -40,11 +42,35 @@ function pageForward() {
     })
 }
 
+function newMonster() {
+    const form = document.querySelector('#form')
+    console.log(form)
+    form.addEventListener("submit", (e) => {
+        e.preventDefault()
+        const monsterObject = {
+            name: `${e.target[0].value}`,
+            age: `${e.target[1].value}`,
+            description: `${e.target[2].value}`
+        }
+        console.log(monsterObject)
+        fetch(`${dataURL}`, {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(monsterObject)
+            }).then(res => res.json().then(data=> console.log(data)))
+        })
+    }
+
 function domLoaded() {
     document.addEventListener('DOMContentLoaded', (e)=> {
         console.log('DOM Loaded')
         getData()
+        newMonster()
         pageForward()
+
     })
 }
 
